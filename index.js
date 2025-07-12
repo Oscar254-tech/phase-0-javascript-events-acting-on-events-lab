ffeee666c1794e8e07fd273cf418339bb0bfc567
oscar ochanda <oscarochanda@gmail.com>
2025-07-12 11:38:48 +0300

    Events-Acting-On-Events-Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game"); 
+
+function moveDodgerLeft() {
+ 
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+  function moveDodgerRight() {
+  
+  const currentLeftStyle = dodger.style.getPropertyValue("left");
+
+  const left = parseFloat(currentLeftStyle);
+
+  const rightBoundary = 360;
+
+  if (left < rightBoundary) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
